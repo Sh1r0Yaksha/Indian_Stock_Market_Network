e8c974508b3eea33b68dcdd0c073a76e470ca766
Sh1r0Yaksha <541r0yak54a@gmail.com>
2024-04-09 23:10:17 +0530

    plots

--- plots.py
+++ plots.py
@@ -3,9 +3,10 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
+colname = 'CH_CLOSING_PRICE'
+df = pd.read_csv('correlation_data/correlation_log_diff_' + colname + '.csv')
 
-def plot_threshold(colname):
-    corr_df = pd.read_csv('correlation_data/correlation_log_diff_' + colname + '.csv')
+def plot_threshold(corr_df):
     G = nx.Graph()
     nodes = set(corr_df['FILENAME1'].unique()) | set(corr_df['FILENAME2'].unique())
     G.add_nodes_from(nodes)
@@ -28,3 +29,48 @@ def plot_threshold(colname):
     plt.title('Edge Density vs. Threshold')
     plt.grid(True)
     plt.show()
+
+plot_threshold(df)
+threshold = 0.25
+
+
+def create_network(corr_df, threshold):
+    G = nx.Graph()
+    nodes = set(corr_df['FILENAME1'].unique()) | set(corr_df['FILENAME2'].unique())
+    G.add_nodes_from(nodes)
+    for _, row in corr_df.iterrows():
+        weight=row['CORRELATION']
+        if weight > threshold and row['FILENAME1'] != row['FILENAME2']:
+            G.add_edge(row['FILENAME1'], row['FILENAME2'], weight=weight)
+    edge_density = (2 * len(G.edges())) / (len(G.nodes()) * (len(G.nodes()) - 1)) # 2E / V(V-1)
+    print(f"Edge density of graph at threshold {threshold} is {edge_density}")
+    return G
+
+
+G = create_network(df, threshold)
+
+def sort_and_print_top_x(df, num, centrality):
+    sorted_df = sorted(df.items(), key=lambda x: x[1], reverse=True)
+
+    print(f"\nTop 10 central nodes based on {centrality} centrality\n")
+    for node, centrality in sorted_df[:num]:
+        print(f"Node {node}: {centrality} Centrality = {centrality:.4f}")
+
+def calculate_centrality_measures(network):
+
+    # Calculate the degree centrality
+    degree_centrality = nx.degree_centrality(network)
+    sort_and_print_top_x(degree_centrality, 10, 'degree')
+
+    # Calculate the betweenness centrality
+    betweenness_centrality = nx.betweenness_centrality(network)
+    sort_and_print_top_x(betweenness_centrality, 10, 'betweenness')
+
+    pagerank = nx.pagerank(network, max_iter=2000)
+    sort_and_print_top_x(pagerank, 10, 'pagerank')
+
+    eigenvector_centrality = nx.eigenvector_centrality(network)
+    sort_and_print_top_x(eigenvector_centrality, 10, 'eigenvector')
+
+calculate_centrality_measures(G)
+
